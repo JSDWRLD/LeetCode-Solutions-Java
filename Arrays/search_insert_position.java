4883a7146d52aa6640b22e108e5d6420453451e2
JSD  ~ [ SafeGuard AI ] <123333887+JSDWRLD@users.noreply.github.com>
2024-04-21 20:50:43 -0700

    Create search_insert_position.java

---
+++ Arrays/search_insert_position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        if(target<=nums[0]) return 0;
+        while(start<=end){
+           int mid = start + (end-start)/2;
+           if(target == nums[mid]) return mid;
+           if(target<nums[mid]) end=mid-1;
+           if(target>nums[mid]) start = mid+1; 
+        }
+
+        // Time Complexity: O(log n)
+        // Space Complexity: O(1)
+       if(end>=0 && target<=nums[end]) return end;
+       else return start;
+    }
+}
