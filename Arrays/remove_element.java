7b29d8e06eec165121853287827bd0c61803ea60
JSD  ~ [ SafeGuard AI ] <123333887+JSDWRLD@users.noreply.github.com>
2024-04-23 20:34:40 -0700

    Create remove_element.java

---
+++ Arrays/remove_element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        int k = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+        return k;
+    }
+}
