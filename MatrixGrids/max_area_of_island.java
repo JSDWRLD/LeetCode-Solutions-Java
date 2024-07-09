ee85e161cf04c08a6e90c8e895c5708e29857885
JSD  ~ [ SafeGuard AI ] <123333887+JSDWRLD@users.noreply.github.com>
2024-07-08 21:04:14 -0700

    Solved #695!

---
+++ MatrixGrids/max_area_of_island.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    int islandArea = dfs(grid, i, j);
+                    max = Math.max(max, islandArea);
+                }
+            }
+        }
+
+        // Time Complexity: O (r * c)
+        // Space Complexity: O (r * c)
+        return max;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) return 0;
+        
+        grid[i][j] = 0;
+
+        return 1 + dfs(grid, i + 1, j) + dfs(grid, i, j + 1) + dfs(grid, i - 1, j) + dfs(grid, i, j - 1);
+
+    }
+}
