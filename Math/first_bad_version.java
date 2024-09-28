c4c68b831e55accf826090db012271a6a353b0e5
JSD  ~ [ SafeGuard AI ] <123333887+JSDWRLD@users.noreply.github.com>
2024-09-27 20:23:01 -0700

    Solved #278!

---
+++ Math/first_bad_version.java
@@ -0,0 +1,21 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid+1;
+            }
+        }
+        
+        // Time Complexity: O(log n)
+        // Space Complexity: O(1)
+        return left;
+    }
+}
