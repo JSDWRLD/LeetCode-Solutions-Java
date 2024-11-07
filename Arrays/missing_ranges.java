67a36ad908f569da6cda4e8d691c35209e2bc042
JSD  ~ [ SafeGuard AI ] <123333887+JSDWRLD@users.noreply.github.com>
2024-11-06 20:51:23 -0800

    Solved #163!

---
+++ Arrays/missing_ranges.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
+        List<List<Integer>> res = new ArrayList<>();
+        int prev = lower;
+
+        for (int curr : nums) {
+            if (curr > prev) {  // if there is a missing range between p and num-1
+                res.add(List.of(prev, curr - 1));
+            }
+            prev = curr + 1;  // move pointer to the next number after num
+        }
+        
+        if (prev <= upper) {  // if there are numbers missing after the last element in nums
+            res.add(List.of(prev, upper));
+        }
+
+        return res;
+    }
+}
