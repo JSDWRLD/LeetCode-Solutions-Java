91e7f806217ad19c43416a59a34484710bce398d
JSD  ~ [ SafeGuard AI ] <123333887+JSDWRLD@users.noreply.github.com>
2024-11-22 20:06:01 -0800

    Solved #1539!

---
+++ Arrays/kth_missing_positive_number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        for (int num : arr) {
+            if (num <= k) {
+                k++;
+            } else {
+                break;
+            }
+        }
+        return k;
+    }
+}
