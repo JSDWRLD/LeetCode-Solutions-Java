f70e44e73524e63dd23601f9af2ccb77feca2334
JSD  ~ [ SafeGuard AI ] <123333887+JSDWRLD@users.noreply.github.com>
2024-12-28 22:02:40 -0800

    Updated

--- Arrays/can_make_arithmetic_progression_from_sequence.java
+++ Arrays/can_make_arithmetic_progression_from_sequence.java
@@ -1,14 +1,29 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        Arrays.sort(arr);
-        int diff = arr[1] - arr[0];
-
-        for (int i = 2; i < arr.length; i++) {
-            if (arr[i] - arr[i-1] != diff) return false;
+        int minValue = Arrays.stream(arr).min().getAsInt();
+        int maxValue = Arrays.stream(arr).max().getAsInt();
+        int n = arr.length;
+        
+        if (maxValue - minValue == 0) {
+            return true;
         }
-
-        // Time Complexity: O(n log n)
-        // Space Complexity: O(1)
-        return true;
+        
+        if ((maxValue - minValue) % (n - 1) != 0) {
+            return false;
+        }
+        
+        int diff = (maxValue - minValue) / (n - 1);
+        Set<Integer> numberSet = new HashSet<>();
+        
+        for (int i = 0; i < n; i++) {
+            if ((arr[i] - minValue) % diff != 0) {
+                return false;
+            }
+            numberSet.add(arr[i]);
+        }
+        
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+        return numberSet.size() == n;
     }
 }
