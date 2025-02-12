59aaec12b7c6516a985b58b83b1c79c352f81bb6
JSD  ~ [ SafeGuard AI ] <123333887+JSDWRLD@users.noreply.github.com>
2025-02-11 17:31:02 -0800

    Solved #2342!

---
+++ Heaps_PQ/max_sum_of_a_pair_with_equal_sum_of_digits.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maximumSum(int[] nums) {
+        if (nums.length < 2) return -1;
+        HashMap<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+        for (int n : nums) {
+            int digitSum = digitSum(n);
+            if (!map.containsKey(digitSum)) {
+                map.put(digitSum, new PriorityQueue<Integer>((a,b) -> b-a));
+            }
+
+            map.get(digitSum).add(n);
+        }
+
+        int max = -1;
+        for (PriorityQueue<Integer> pq : map.values()) {
+            if (pq.size() > 1) {
+                int temp = pq.poll();
+                temp +=  + pq.poll();
+                max = Math.max(max, temp);
+            }
+        }
+
+        // Time Complexity: O(n log n)
+        // Space Complexity: O(d * n)
+        return max;
+    }
+    
+    private int digitSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
