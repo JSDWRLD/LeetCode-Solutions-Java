3000b2cdea1b7fecec16f6dc54f6137e8bdd397f
John Son Dong <123333887+JSDWRLD@users.noreply.github.com>
2025-05-27 20:07:25 -0700

    Solved #1047!

---
+++ Strings/remove_all_adjacent_duplicates_in_string.java
@@ -0,0 +1,18 @@
+class Solution {
+  public String removeDuplicates(String S) {
+    StringBuilder sb = new StringBuilder();
+    int sbLength = 0;
+    for(char character : S.toCharArray()) {
+      if (sbLength != 0 && character == sb.charAt(sbLength - 1))
+        sb.deleteCharAt(sbLength-- - 1);
+      else {
+        sb.append(character);
+        sbLength++;
+      }
+    }
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+    return sb.toString();
+  }
+}
